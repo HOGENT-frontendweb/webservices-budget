017a4a1b3f1bf3dc9724c3b8ea8473dc9461b827
Thomas Aelbrecht <thomas.aelbrecht@hogent.be>
2025-12-04 16:25:12 +0100

    Fix typo in get place by id

--- src/place/place.service.ts
+++ src/place/place.service.ts
@@ -38,6 +38,7 @@ export class PlaceService {
             user: {
               columns: {
                 id: true,
+                name: true,
                 email: true,
               },
             },
