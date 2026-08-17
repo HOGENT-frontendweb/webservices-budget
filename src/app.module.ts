53dfcefc54d23209ae1884f85684ce9acace483c
dreeki <andreas.dewitte@hotmail.com>
2026-08-17 16:12:56 +0200

    Hoofdstuk 2: [Niet volgens cursus] opkuis AppController, AppService en AppService.spec

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { HealthController } from './health/health.controller';
 import { PlaceModule } from './place/place.module';
 
 @Module({
   imports: [PlaceModule],
-  controllers: [AppController, HealthController],
-  providers: [AppService],
+  controllers: [HealthController],
+  providers: [],
 })
 export class AppModule {}
