2022b40a602ee072130dfd5a5a169fb0ff95d442
dreeki <andreas.dewitte@hotmail.com>
2026-09-02 12:06:11 +0200

    Hoofdstuk 6: public en roles guards toegevoegd

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,10 +4,23 @@ import { PlaceModule } from '../place/place.module';
 import { UserService } from './user.service';
 import { DrizzleModule } from '../drizzle/drizzle.module';
 import { AuthModule } from '../auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 @Module({
   imports: [AuthModule, DrizzleModule, PlaceModule],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+    UserService,
+  ],
 })
 export class UserModule {}
