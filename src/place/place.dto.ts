26a61a1a07b0f539f541a5d2a24174d0a542f788
dreeki <andreas.dewitte@hotmail.com>
2026-09-02 13:16:17 +0200

    Hoofdstuk 7: Place DTO documenteren

--- src/place/place.dto.ts
+++ src/place/place.dto.ts
@@ -1,31 +1,43 @@
 import { TransactionResponseDto } from '../transaction/transaction.dto';
-import {
-  IsInt,
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-  Max,
-  MaxLength,
-  Min,
-} from 'class-validator';
+import { IsNumber, IsString } from 'nestjs-swagger-dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { Place, CreatePlace } from '../types/place';
 
-export class CreatePlaceRequestDto {
-  @IsString()
-  @IsNotEmpty()
-  @MaxLength(255)
+export class CreatePlaceRequestDto implements CreatePlace {
+  @IsString({ name: 'name', maxLength: 255 })
   name: string;
 
-  @IsOptional()
-  @IsInt()
-  @Min(1)
-  @Max(5)
-  rating: number | null;
+  @IsNumber({
+    name: 'rating',
+    min: 1,
+    max: 5,
+    optional: true,
+    format: 'int32',
+    type: 'integer',
+  })
+  rating?: number;
 }
 
 export class UpdatePlaceRequestDto extends CreatePlaceRequestDto {}
 
-export class PlaceResponseDto extends CreatePlaceRequestDto {
+export class PlaceResponseDto implements Place {
+  @ApiProperty({ example: 1, description: 'ID of the place' })
   id: number;
+
+  @ApiProperty({
+    example: 'Loon',
+    description: 'Name of the place where transactions can occur',
+  })
+  name: string;
+
+  @ApiProperty({
+    example: 4,
+    description: 'Rating of the place (1 to 5)',
+    nullable: true,
+    format: 'int32',
+    type: 'integer',
+  })
+  rating: number | null;
 }
 
 export class PlaceListResponseDto {
@@ -33,5 +45,6 @@ export class PlaceListResponseDto {
 }
 
 export class PlaceDetailResponseDto extends PlaceResponseDto {
+  @ApiProperty({ type: () => [TransactionResponseDto] })
   transactions: TransactionResponseDto[];
 }
