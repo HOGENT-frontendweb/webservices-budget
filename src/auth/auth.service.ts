eee66836ca8bee0c85e868b441bf03d9000f2052
dreeki <andreas.dewitte@hotmail.com>
2026-09-02 11:30:36 +0200

    Hoofdstuk 6: password hash en verify

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,9 +3,10 @@ import {
   type DatabaseProvider,
   InjectDrizzle,
 } from '../drizzle/drizzle.provider';
+import * as argon2 from 'argon2';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
-import { ServerConfig } from '../config/configuration';
+import { AuthConfig, ServerConfig } from '../config/configuration';
 
 @Injectable()
 export class AuthService {
@@ -15,4 +16,18 @@ export class AuthService {
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService<ServerConfig>,
   ) {}
+
+  async hashPassword(password: string): Promise<string> {
+    const authConfig = this.configService.get<AuthConfig>('auth')!;
+    return argon2.hash(password, {
+      type: argon2.argon2id,
+      hashLength: authConfig.hashLength,
+      timeCost: authConfig.timeCost,
+      memoryCost: authConfig.memoryCost,
+    });
+  }
+
+  async verifyPassword(password: string, hash: string): Promise<boolean> {
+    return await argon2.verify(hash, password);
+  }
 }
