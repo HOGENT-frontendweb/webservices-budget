7939be895e16dc4a491942334fd077e4fb408f82
dreeki <andreas.dewitte@hotmail.com>
2026-09-02 13:28:41 +0200

    Hoofdstuk 7: Login endpoint documenteren

--- src/session/session.controller.ts
+++ src/session/session.controller.ts
@@ -10,11 +10,28 @@ import { AuthService } from '../auth/auth.service';
 import { LoginRequestDto, LoginResponseDto } from './session.dto';
 import { Public } from '../auth/decorators/public.decorator';
 import { AuthDelayInterceptor } from '../auth/interceptors/authDelay.interceptor';
+import {
+  ApiBadRequestResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
+@ApiTags('Sessions')
 @Controller('sessions')
 export class SessionController {
   constructor(private authService: AuthService) {}
 
+  @ApiOkResponse({
+    description: 'Login',
+    type: LoginResponseDto,
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Invalid credentials',
+  })
+  @ApiBadRequestResponse({
+    description: 'Invalid input data',
+  })
   @UseInterceptors(AuthDelayInterceptor)
   @Public()
   @Post()
