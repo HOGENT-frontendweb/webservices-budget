03ffd290883185ec80cc084bdd9794a982db837e
dreeki <andreas.dewitte@hotmail.com>
2026-09-02 10:40:08 +0200

    Hoofdstuk 5: configuratie logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { HealthController } from './health/health.controller';
 import { PlaceModule } from './place/place.module';
 import { ConfigModule } from '@nestjs/config';
@@ -6,6 +6,7 @@ import { DrizzleModule } from './drizzle/drizzle.module';
 import { TransactionModule } from './transaction/transaction.module';
 import { UserModule } from './user/user.module';
 import configuration from './config/configuration';
+import { LoggerMiddleware } from './lib/logger.middleware';
 
 @Module({
   imports: [
@@ -21,4 +22,8 @@ import configuration from './config/configuration';
   controllers: [HealthController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*path');
+  }
+}
