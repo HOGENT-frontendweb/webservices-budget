0edb34b82c1809fbf8306dd8608119e271cf915f
dreeki <andreas.dewitte@hotmail.com>
2026-09-02 11:29:49 +0200

    Hoofdstuk 6: auth service + configuratie initial opzet

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { DrizzleModule } from '../drizzle/drizzle.module';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { ServerConfig, AuthConfig } from '../config/configuration';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      global: true,
+      useFactory: (configService: ConfigService<ServerConfig>) => {
+        const authConfig = configService.get<AuthConfig>('auth')!;
+
+        return {
+          secret: authConfig.jwt.secret,
+          signOptions: {
+            expiresIn: `${authConfig.jwt.expirationInterval}s`,
+            audience: authConfig.jwt.audience,
+            issuer: authConfig.jwt.issuer,
+          },
+          verifyOptions: {
+            audience: authConfig.jwt.audience,
+            issuer: authConfig.jwt.issuer,
+          },
+        };
+      },
+    }),
+    DrizzleModule,
+  ],
+  providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import {
+  type DatabaseProvider,
+  InjectDrizzle,
+} from '../drizzle/drizzle.provider';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { ServerConfig } from '../config/configuration';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectDrizzle()
+    private readonly db: DatabaseProvider,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService<ServerConfig>,
+  ) {}
+}
