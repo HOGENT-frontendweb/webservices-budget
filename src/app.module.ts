0343d0732bffcfdda8fa1afcaae4c625a0002066
dreeki <andreas.dewitte@hotmail.com>
2026-09-02 11:28:48 +0200

    Hoofdstuk 6: auth module + jwt dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { TransactionModule } from './transaction/transaction.module';
 import { UserModule } from './user/user.module';
 import configuration from './config/configuration';
 import { LoggerMiddleware } from './lib/logger.middleware';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { LoggerMiddleware } from './lib/logger.middleware';
     DrizzleModule,
     TransactionModule,
     UserModule,
+    AuthModule,
   ],
   controllers: [HealthController],
   providers: [],
