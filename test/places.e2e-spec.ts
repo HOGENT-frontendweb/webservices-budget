7e27042b378a15dce64fbda1b2142cea7c8c3894
dreeki <andreas.dewitte@hotmail.com>
2026-09-04 12:11:42 +0200

    Hoofdstuk 8: Places test met authenticatie

--- test/places.e2e-spec.ts
+++ test/places.e2e-spec.ts
@@ -6,10 +6,14 @@ import {
 import { createTestApp } from './helpers/create-app';
 import { PLACES_SEED, seedPlaces, clearPlaces } from './seeds/places';
 import request from 'supertest';
+import { clearUsers, seedUsers } from './seeds/users';
+import { login, loginAdmin } from './helpers/login';
 
 describe('Places', () => {
   let app: INestApplication;
   let drizzle: DatabaseProvider;
+  let userAuthToken: string;
+  let adminToken: string;
 
   const url = '/api/places';
 
@@ -18,16 +22,23 @@ describe('Places', () => {
     drizzle = app.get(DrizzleAsyncProvider);
 
     await seedPlaces(drizzle);
+    await seedUsers(app, drizzle);
+
+    userAuthToken = await login(app);
+    adminToken = await loginAdmin(app);
   });
 
   afterAll(async () => {
     await clearPlaces(drizzle);
+    await clearUsers(drizzle);
     await app.close();
   });
 
   describe('GET /api/places', () => {
     it('should 200 and return all places', async () => {
-      const response = await request(app.getHttpServer()).get(url);
+      const response = await request(app.getHttpServer())
+        .get(url)
+        .auth(userAuthToken, { type: 'bearer' });
 
       expect(response.statusCode).toBe(200);
       expect(response.body.items).toEqual(expect.arrayContaining(PLACES_SEED));
