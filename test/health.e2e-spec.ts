ef368343a63fd0749009f33a4c1e0d466e3e034f
dreeki <andreas.dewitte@hotmail.com>
2026-09-04 11:48:38 +0200

    Hoofdstuk 8: Health controller test refactor

--- test/health.e2e-spec.ts
+++ test/health.e2e-spec.ts
@@ -1,19 +1,12 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import { AppModule } from '../src/app.module';
 import request from 'supertest';
+import { createTestApp } from './helpers/create-app';
 
 describe('Health', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    app.setGlobalPrefix('api');
-    await app.init();
+    app = await createTestApp();
   });
 
   afterAll(async () => {
